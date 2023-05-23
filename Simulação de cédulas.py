af59daa4f0bed997cf41121f16a7adc8a8c454ad
Átila <112980460+Relieff@users.noreply.github.com>
2023-05-22 23:44:37 -0300

    Update Simulação de cédulas.py

--- Simulação de cédulas.py
+++ Simulação de cédulas.py
@@ -18,43 +18,43 @@ print('Carregamento completo! Bem vindo(a)!')
 print('-'*20)
 sleep(1)
 
-dinheiro = 0
-total = dinheiro
-totalced = 0
-cedula = 50
+dinheiro=0
+total=dinheiro
+totalced=0
+cedula=50
 
 
 while True:
-    dinheiro = int(input('Digite o valor em R$: '))
+    dinheiro=int(input('Digite o valor em R$: '))
     print('')
-    resp = str(input('Deseja finalizar a operação? [S/N] ')).strip().upper()[0]
+    resp=str(input('Deseja finalizar a operação? [S/N] ')).strip().upper()[0]
     print('')
 
 
-    if total >= cedula:
-        total -= cedula
-        totalced += 1
+    if total>=cedula:
+        total-=cedula
+        totalced+=1
     else:
 
         print(f'O dinheiro sacado foi de R${dinheiro} sendo {totalced} cedulas de {cedula}')
-        if cedula == 50:
-            cedula = 20
-        elif cedula == 20:
-            cedula = 10
-        elif cedula == 10:
-            cedula = 1
-        totalced = 0
-        if total == 0:
+        if cedula==50:
+            cedula=20
+        elif cedula==20:
+            cedula=10
+        elif cedula==10:
+            cedula=1
+        totalced=0
+        if total==0:
             break
 
-    if resp == 'S':
+    if resp=='S':
         print('Encerrando operação . . .')
         sleep(1)
         break
                 #Depois de fazer a primeira parte do programa ele pula pra segunda
-    op = str(input('Deseja corrigir a operação? [S/N] ')).strip().upper()[0]
+    op=str(input('Deseja corrigir a operação? [S/N] ')).strip().upper()[0]
 
-    if op == 'N':
+    if op=='N':
         print('Encerrando operação . . .')
         sleep(1)
         break
